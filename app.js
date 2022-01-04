4c7ac6d6f20bd3e29e660f98cd7d5e89e0ab5e9f
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2022-01-04 11:19:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-require("dotenv").config();                              //require the dotenv package to use environmental variables 
+
 
 const express = require("express");
 const mongoose = require("mongoose");
